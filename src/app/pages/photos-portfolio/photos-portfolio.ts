7c4229bfb82061b1b3ff273954c95b311f559664
Diego de Castro Merillas <diegodecastro@usal.es>
2026-02-13 19:26:51 +0100

    added new part of photos

--- src/app/pages/photos-portfolio/photos-portfolio.ts
+++ src/app/pages/photos-portfolio/photos-portfolio.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { Carrousel } from "../../components/carrousel/carrousel";
+import { ImageGrid } from "../../components/image-grid/image-grid";
 
 @Component({
   selector: 'app-photos-portfolio',
-  imports: [Carrousel],
+  imports: [Carrousel, ImageGrid],
   templateUrl: './photos-portfolio.html',
   styleUrl: './photos-portfolio.css',
 })
